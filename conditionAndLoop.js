368d3ae0426c5a9a0567451d719502d57baabc92
AkashDhere18 <dhereakash18@gmail.com>
2026-03-12 22:55:18 +0530

    solved ass no 15 to 19 in conditionAndLoop.js

--- conditionAndLoop.js
+++ conditionAndLoop.js
@@ -23,7 +23,7 @@
 
 // }
 
-//Ass 1
+//                  Ass 1  :-  No cheaker + - or 0
 
 // let num = prompt("Enter no to chech +ve or -ve");
 
@@ -34,7 +34,9 @@
 //     console.log(num ,"is positive");
 // }
 
-//Ass 2
+
+//                  Ass 2  :- Multiplication table
+
 
 // let no = prompt("Enter no to get table")
 
@@ -42,7 +44,9 @@
 //     console.log(no ,'x',i,'=',no*i )
 // }
 
-//Ass 3
+
+//                  Ass 3  :- No gusseing game
+
 // let randomNo =Math.floor( Math.random()*10);
 // console.log(randomNo);
 // console.log("guess no in 3 chance");
@@ -63,14 +67,18 @@
 
 // console.log("Out of attempts (loss) ")
 
-//Ass 4
+
+//                  Ass 4  :- Print even no
+
 // for(i=1 ; i<=20; i++){
 //     if(i%2 == 0){
 //         console.log(i);
 //     }
 // }
 
-//Ass 5
+
+//                  Ass 5  :- Sum of digits
+
 
 // let n=1235;
 // let t=0;
@@ -86,7 +94,9 @@
 
 // console.log(t);
 
-//Ass 6
+
+//                  Ass 6  :- FizzBuzz problem
+
 
 // for(let i=0; i<=50; i++){
 
@@ -104,7 +114,8 @@
 //     }
 // }
 
-//Ass 7
+
+//                  Ass 7  :- Prime no cheaker
 // let n=99;
 // let no = 0;
 
@@ -122,7 +133,7 @@
 //  }
 
 
-//Ass 9
+//                  Ass 9  :- Find largest of 3 no
 // let n1 =24;
 // let n2 =121;
 // let n3 =191;
@@ -137,7 +148,8 @@
 //     console.log("greater is n3:",n3);
 // }
 
-//Ass 11
+
+//                  Ass 11  :- Count vowels and consonents
 
 // let str = "Hello Javascript";
 // let vowles = "aeiouAEIOU"
@@ -152,7 +164,9 @@
 // console.log("Vowels =", vowelsCount);
 // console.log("Consonents =", strModified.length - vowelsCount);
 
-//Ass 12
+
+//                  Ass 12  :- Reverse a No
+
 // let num = 12345;
 // let numStr = num.toString();
 // let reverseNum ="";
@@ -165,7 +179,8 @@
 // console.log(Number(reverseNum));
 
 
-//Ass 13
+//                  Ass 13  :- Sum of odd and even no
+
 
 // let sumofOdd =0;
 // let sumofEven =0;
@@ -183,7 +198,8 @@
 // console.log("Sum of odd no =",sumofOdd);
 
 
-//Ass 14
+//                  Ass 14  :- Factorial using while loop
+
 
 // let num=5;
 // let factorial = 1;
@@ -194,3 +210,86 @@
 // }
 
 // console.log(factorial);
+
+
+//                  Ass 15  :- Average of n numbers
+
+
+// let numbers=[50,10,20,15]
+// let sum=0;
+// for(i=0; i<=numbers.length-1; i++){
+//     sum+=numbers[i];
+// }
+
+// console.log(sum)
+
+
+//                  Ass 16  :- Leap year cheaker
+
+
+// let year=1900
+
+// if(year%4 === 0 && year%100 !== 0 || year%400 === 0){
+//     console.log("Leap Year")
+// }
+// else{
+//     console.log("Not Leap Year")
+// }
+
+
+//                  Ass 17  :- Divisors of no
+
+// let num = 12;
+// let divisors ="";
+
+// for(i=1; i<=num; i++){
+//     if(num%i === 0){
+//         divisors = divisors + " " +i
+//     }
+// }
+
+// console.log("dividors are:",divisors)
+
+
+//                  Ass 18  :- Fibonacci sequence
+
+
+// let n=20;
+
+// let n1 = 0;
+// let n2 =1;
+
+// let sequence="0,1"
+// while((n-2)>=1){
+
+//     n3 = n1+n2
+    
+//     n1=n2;
+//     n2=n3;
+    
+//     sequence+= ","+n3
+//     n--
+// }
+// console.log(sequence)
+
+//                  Ass 19  :- Armstrong No cheaker
+
+// let num = 10;
+// let numStr=num.toString()
+// let len1 =numStr.length
+// let digitsSum =0;
+
+// for(i=0; i<len1; i++){
+//     // console.log(numStr.charAt(i))
+//     strTOnum = parseInt(numStr.charAt(i))
+//     // console.log(typeof(strTOnum))
+//     digitsSum+= Math.pow(strTOnum,len1)
+//     // console.log(digitsSum)
+// }
+
+// if(digitsSum == num){
+//     console.log(num,"is a Armstrong No.")
+// }
+// else{
+//     console.log(num,"is not a Armstrong No.")
+// }
