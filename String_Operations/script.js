d806f01ea7c1b310f273491c91db309d53d4bd90
AkashDhere18 <dhereakash18@gmail.com>
2026-03-14 23:49:42 +0530

    In string operations added three functions of string operations numberOfOccurance(), stringToArray(), firstWord_ofSentence()

--- String_Operations/script.js
+++ String_Operations/script.js
@@ -36,12 +36,30 @@ function palindromChek(){
     }
 }
 
+function firstWord_ofSentence(){
+    inputValue = inputElement.value
+    inputValue_splited = inputValue.split(" ")
+    // console.log(inputValue_splited) 
+
+    firstWord = inputValue_splited[0]
+    console.log(firstWord)
+
+    return outputElement.innerHTML = "First word of Sentence is :" + firstWord;
+}
+
 function replaceSpace(){
     inputValue =inputElement.value;
 
     result = inputValue.replaceAll(" "  , "_")
 
     outputElement.innerHTML = result;
+}
+
+function stringToArray(){
+    inputValue = inputElement.value;
+    newArray = inputValue.split(" ")
+
+    outputElement.innerHTML = "Generated Array is :" + newArray
 }
 
 function string_start_end_with_sameChar(){
@@ -63,3 +81,20 @@ function string_start_end_with_sameChar(){
 //     let updatedValue = inputValue.toTitleCase()
 //     console.log(updatedValue)
 // }
+
+function numberOfOccurance(){
+    inputValue = inputElement.value
+    specificWord = prompt("Enter the character to count no of occurance")
+    // console.log(specificWord)
+    toArray = inputValue.split(" ")
+    // console.log(toArray)
+    occurance= 0;
+
+    for(i=0; i<=toArray.length-1; i++){
+        if(toArray[i] === specificWord){
+            occurance++
+        }
+    }
+
+    return outputElement.innerHTML = `No of occurance of ${specificWord} :` + occurance
+}
