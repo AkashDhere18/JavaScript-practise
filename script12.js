c90dbf0fe130430dd15325f09dcbe327387fb467
AkashDhere18 <dhereakash18@gmail.com>
2026-03-16 22:13:14 +0530

    created a search in array , and filter even odd project using java script add files  in arrays practice folder

---
+++ script12.js
@@ -0,0 +1,4 @@
+// InputArray = ['peacock','mahgo','Building','blue','Board','street','Billing']
+// // console.log("Hello")
+// OutputA  = InputArray.filter((value) =>  value.startsWith( "B") || value.startsWith("b"))
+// console.log(OutputA)
