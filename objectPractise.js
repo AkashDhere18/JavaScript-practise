6f5765aa2cf5d8d37da2ff4e32a4b3c66afbf384
AkashDhere18 <dhereakash18@gmail.com>
2026-03-17 18:02:53 +0530

    added objectPractise.js file ,practised objects by solving assignments

---
+++ objectPractise.js
@@ -0,0 +1,70 @@
+
+// ##Ass-1
+
+
+// const student = {
+//     name:"john",
+//     age:18,
+//     grade:"A"
+// }
+
+// student.subject = "maths";
+// console.log(student)
+
+// student.grade ="A+"
+// console.log(student)
+
+// delete student.age
+// console.log(student)
+
+
+//##Ass-2
+
+// const book ={
+//     title:'Atomic Habits',
+//     authoe: "xyz",
+//     details : {
+//         pages : 300,
+//         gener : 'Motivational'
+//     }
+// }
+
+// console.log(book.details.pages)
+// console.log(book.details.gener)
+
+
+//##Ass-3
+
+// const product = {
+//     name : "T-Shirt",
+//     price:"10 $",
+//     stock:9
+// }
+
+// for(i in product){
+//     console.log(`${i}=${product[i]}`)
+// }
+
+//##Ass-4
+
+// const calculator = {
+//     n1:10,
+//     n2:5,
+//     add: function (){
+//         console.log("Addition :",this.n1 + this.n2)
+//     },
+//     sub: function (){
+//         console.log("Subtraction : " , this.n1 - this.n2)
+//     },
+//     multi: function (){
+//         console.log("Multiplication : " , this.n1 * this.n2)
+//     },
+//     divide: function (){
+//         console.log("Division : " , this.n1 / this.n2)
+//     }
+// }
+
+// calculator.add()
+// calculator.sub()
+// calculator.multi()
+// calculator.divide()
