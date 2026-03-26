225c32fcc0bac842fa3dbfde236b1c2b9ffdd94c
AkashDhere18 <dhereakash18@gmail.com>
2026-03-26 23:48:01 +0530

    completed two tasks count no of words in sentence and find longest word in string using js

---
+++ TasksUsing JS/TasksUsingJs.js
@@ -0,0 +1,45 @@
+// ****#Task 1 count no of words in sentence#****
+
+// let str = "this is the sentence to count the no of words in sentence"
+
+// function countWords(){
+//     str = str.split(" ")
+//     // console.log(str)
+//     count = 0
+//     for(i=0; i<str.length; i++){
+//         if(str[i] != ""){
+//             count++
+//         }
+//     }
+//     return count
+// }
+
+// console.log("No of Word's are : ",countWords())
+
+// ****#Task to find longest word in string#****
+
+// let str = "this is string to find longest word paracetomol in string"
+
+// function longestWord(){
+//     str = str.split(" ")
+
+//     let maxN = str[0].length;
+//     let longestWord = str[0];
+//     for(i=0; i<str.length; i++){
+
+//         if(str[i].length > maxN){
+//             maxN = str[i].length
+//             longestWord = str[i]
+//         }
+
+//     }
+
+//     return console.log("Longest word is " + longestWord + " length is " + maxN)
+// }
+
+// longestWord()
+
+//****#Task find second largest no in array#****
+
+let arr1 = [55,46,57,23,85,65,54,47,45,12,65]
+
